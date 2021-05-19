3aae87f0c9f5c4a32a870b62348817dd77617818
agrufu <69220569+agrufu@users.noreply.github.com>
2021-05-20 00:35:50 +0200

    Add files via upload

--- js/CalcularMedia.js
+++ js/CalcularMedia.js
@@ -18,13 +18,54 @@ function CalcularMedia() {
     document.getElementById("media").value = media;
 
     // Se muestran los datos por consola
-    console.log(parseFloat(prog));
-    console.log(parseFloat(bbdd));
-    console.log(parseFloat(eedd));
-    console.log(parseFloat(llmm));
+    console.log("- Programación: " + parseFloat(prog));
+    console.log("- Bases de Datos: " + parseFloat(bbdd));
+    console.log("- Entornos de Desarrollo: " + parseFloat(eedd));
+    console.log("- Lenguajes de Marcas: " + parseFloat(llmm));
 
-    // Se muesta el resultado del cálculo por consola
-    console.log(calculo);
-    console.log(media);
+    // Se muestra el resultado del cálculo por consola
+    console.log("******************************");
+    console.log("Suma total de notas: " + calculo);
+    console.log("La media del curso es: " + media);
+  
+    if (prog < 5) {
+        var prog_result = "-Programación: " + "\t\t SUSPENDIDA\n";
+        console.log(prog_result);        
+    }else{
+        prog_result = "-Programación: " + "\t\t APROBADA\n";
+        console.log(prog_result);
+    }
+    if (bbdd < 5) {
+        var bbdd_result = "-Bases de Datos: " + "\t SUSPENDIDA\n";
+        console.log(bbdd_result);        
+    }else{
+        bbdd_result = "-Bases de Datos: " + "\t\t APROBADA\n";
+        console.log(bbdd_result);
+    }
+    if (eedd < 5) {
+        var eedd_result = "-Entornos de Desarrollo: " + "SUSPENDIDA\n";
+        console.log(eedd_result);        
+    }else{
+        eedd_result = "-Entornos de Desarrollo: " + "\t APROBADA\n";
+        console.log(eedd_result);
+    }
+    if (llmm < 5) {
+        var llmm_result = "-Lenguajes de Marcas: " + "\t SUSPENDIDA\n";
+        console.log(llmm_result);        
+    }else{
+        llmm_result = "-Lenguajes de Marcas: " + "\t\APROBADA\n";
+        console.log(llmm_result);
+    }
+
+    if (prog < 5 || bbdd < 5 || eedd < 5 || llmm < 5){
+        var verano = "\n  Este verano hay que esforzarse más.";
+        console.log(verano);
+    }else{
+        verano = "\n  ¡¡¡Este verano de fiestaaa!!!";
+        console.log(verano);
+    }
+
+    document.getElementById("textarea").value = prog_result + bbdd_result + eedd_result + llmm_result + verano;
+
     return false;
 }
